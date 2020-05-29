54abc08cc21fd6affb9ce198753aeca77686ad48
NazarAvilin <avilin1@outlook.com>
2020-05-29 19:27:10 +0500

    0.3. Тестирование исключений с junit [#282228]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -3,10 +3,10 @@ package ru.job4j.ex;
 public class Fact {
 
     public static void main(String[] args) {
-        System.out.println(new Fact().calc(4));
+        System.out.println(calc(4));
     }
 
-    public int calc(int n) {
+    public static int calc(int n) {
         int rsl = 1;
         if (n < 0) {
             throw new IllegalArgumentException("Параметр 'n' должен быть больше нуля!");
