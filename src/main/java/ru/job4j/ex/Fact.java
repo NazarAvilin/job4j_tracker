3bdae6f6ab459155afac526e759d0fd59788350d
NazarAvilin <avilin1@outlook.com>
2020-05-29 18:53:24 +0500

    0.2. Кидаем исключение - throw new RuntimeException [#282227]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        System.out.println(new Fact().calc(4));
+    }
+
+    public int calc(int n) {
+        int rsl = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException("Параметр 'n' должен быть больше нуля!");
+        } else {
+            for (int index = 1; index <= n; index++) {
+                rsl *= index;
+            }
+        }
+        return rsl;
+    }
+}
