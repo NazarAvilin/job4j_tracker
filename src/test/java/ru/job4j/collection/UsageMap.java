1ddc95ebf6c584f63611cac5ee5fa5fd07bf6397
NazarAvilin <avilin1@outlook.com>
2020-06-08 15:56:26 +0500

    0. Map, HashMap [#282195]

---
+++ src/test/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
+        map.put("avilin1@outlook.com", "Avilin Nazar Markovich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " : " + value);
+        }
+
+    }
+
+}
+
