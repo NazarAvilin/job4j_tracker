ad66d7ade0d70430023f9b42830f460f7f701b55
NazarAvilin <avilin1@outlook.com>
2020-06-12 13:24:01 +0500

    Отсортировать департаменты [#282178]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int countre = Math.min(left.length(), right.length());
+        int rsl = 0;
+        for (int i = 0; i < countre; i++) {
+            rsl = Character.compare(left.charAt(i), right.charAt(i));
+            if (rsl != 0) {
+                break;
+            }
+        }
+        if (rsl == 0) {
+            rsl = left.length() - right.length();
+        }
+        return rsl;
+    }
+}
