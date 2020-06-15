dbda27b9437a9b492cd69b2ad2eab6e30d1e8fc5
NazarAvilin <avilin1@outlook.com>
2020-06-15 18:37:55 +0500

    Отсортировать департаменты [#282178]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -14,14 +14,30 @@ public class DepDescComp implements Comparator<String> {
         int rsl = 0;
         for (int i = 0; i < counter; i++) {
             int compare = left.get(i).compareTo(right.get(i));
+
             if (compare != 0) {
                 rsl = compare;
                 break;
             }
         }
-        if (rsl == 0) {
-            rsl = left.size() - right.size();
-        }
-        return rsl;
+        return rsl == 0 ? left.size() - right.size() : rsl;
     }
+
+    //не правелно!
+//    @Override
+//    public int compare(String o1, String o2) {
+//        String first = o1.split("/")[0];
+//        String second = o2.split("/")[0];
+//        int counter = Math.min(first.length(), second.length());
+//        int rsl = 0;
+//        for (int i = 0; i < counter; i++) {
+//            int compare = Integer.compare(first.length(), second.length());
+//            if (compare != 0) {
+//                rsl = compare;
+//                break;
+//            }
+//        }
+//        return rsl == 0 ? first.length() - second.length() : rsl;
+//    }
+
 }
