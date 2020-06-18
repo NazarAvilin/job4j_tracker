0dfaa55f53d5591d44a4dcd152483e167c6ef750
NazarAvilin <avilin1@outlook.com>
2020-06-18 20:49:22 +0500

    4. Преобразование List в Map. [#282054]

--- src/test/java/ru/job4j/lambda/SchoolTest.java
+++ src/test/java/ru/job4j/lambda/SchoolTest.java
@@ -10,13 +10,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class SchoolTest {
     private final List<Student> students = Arrays.asList(new Student(50),
             new Student(30), new Student(45), new Student(100),
-            new Student(65), new Student(80));
+            new Student(65), new Student(80), new Student(45));
 
     @Test
     public void whenScoreFromZeroToFifty() {
         List<Student> result = School.collect(students,
                 st -> st.getScore() < 50 && st.getScore() >= 0);
-        assertThat(result, is(Arrays.asList(new Student(30), new Student(45))));
+        assertThat(result, is(List.of(new Student(30), new Student(45), new Student(45))));
     }
 
     @Test
