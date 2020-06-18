ede21eb2e5cac863798639db60b244eff21172d1
NazarAvilin <avilin1@outlook.com>
2020-06-18 17:42:21 +0500

    8. Перенос методов в Stream API. [#282058]

--- src/main/java/ru/job4j/lambda/School.java
+++ src/main/java/ru/job4j/lambda/School.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class School {
     public static List<Student> collect(final List<Student> students, final Predicate<Student> predict) {
@@ -12,5 +13,13 @@ public final class School {
 
     public Map<String, Student> collectInMap(List<Student> students) {
         return students.stream().collect(Collectors.toMap(Student::getSurname, st -> st));
+    }
+
+    public List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .flatMap(Stream::ofNullable)
+                .sorted()
+                .filter(student -> student.getScore() > bound)
+                .collect(Collectors.toList());
     }
 }
