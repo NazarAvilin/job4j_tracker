388868da22f588549a3beb895f9227d38532c6d9
NazarAvilin <avilin1@outlook.com>
2020-06-19 15:42:35 +0500

    4. Преобразование List в Map. [#282054]

--- src/test/java/ru/job4j/lambda/SchoolTest.java
+++ src/test/java/ru/job4j/lambda/SchoolTest.java
@@ -1,12 +1,13 @@
 package ru.job4j.lambda;
 
 import org.junit.Test;
-import static org.hamcrest.core.Is.is;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 
 public class SchoolTest {
@@ -37,9 +38,17 @@ public class SchoolTest {
     }
 
     @Test
-    public void whenMapTest() {
-        Map<String, Student> result = students.stream().collect(Collectors.toMap(Student::getSurname, st -> st));
-        assertThat(result, is(List.of(new Student("Max", 45))));
+    public void whenConvertedListToMap() {
+        List<Student> students = List.of(
+                new Student("Ivan", 50),
+                new Student("Vasya", 80),
+                new Student("Petr", 90),
+                new Student("Juri", 30)
+        );
+        Map<String, Student> rsl = new School().collectInMap(students);
+        for (Map.Entry < String, Student > studentMap : rsl.entrySet()) {
+            System.out.println(studentMap.getKey() + " " + studentMap.getValue().toString());
+        }
+        assertThat(rsl, hasEntry("Petr", new Student("Petr", 90)));
     }
-
 }
